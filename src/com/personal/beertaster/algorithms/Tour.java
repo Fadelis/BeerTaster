09c6fdfb38132f861a9ee5cd69981eef4b0f1664
Paulius P <paulius@satalia.com>
2016-12-01 16:23:04 +0200

    changes tour distance calculation ot streams

--- src/com/personal/beertaster/algorithms/Tour.java
+++ src/com/personal/beertaster/algorithms/Tour.java
@@ -1,11 +1,13 @@
 package com.personal.beertaster.algorithms;
-import java.util.ArrayList;
-import java.util.Collections;
 
 import com.personal.beertaster.elements.Beer;
 import com.personal.beertaster.elements.Brewery;
 
-public class Tour{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class Tour {
 
 
     private ArrayList<Brewery> tour = new ArrayList<>();
@@ -13,49 +15,50 @@ public class Tour{
     private double fitness = 0;
     private double distance = 0;
     private int beerCount = 0;
-    
 
-    public Tour(){ }
-    
-    public Tour(ArrayList<Brewery> tour){
+
+    public Tour() {
+    }
+
+    public Tour(ArrayList<Brewery> tour) {
         this.tour = tour;
     }
-    
-    public Tour(Tour tour){
-    	if (tour != null && tour.tour != null)
-    		this.tour.addAll(tour.tour);
+
+    public Tour(Tour tour) {
+        if (tour != null && tour.tour != null)
+            this.tour.addAll(tour.tour);
     }
-    
+
     public void setTour(Tour tour) {
-    	if (tour != null && tour.tour != null) {
-    		this.tour.clear();
+        if (tour != null && tour.tour != null) {
+            this.tour.clear();
             this.tour.addAll(tour.tour);
             fitness = 0;
             distance = 0;
             beerCount = 0;
-    	}
+        }
     }
 
     // Creates a random individual
     public void generateIndividual() {
         // Loop through all our destination cities and add them to our tour
         for (int breweryIndex = 0; breweryIndex < BreweryManager.getBreweryNumber(); breweryIndex++) {
-          setBrewery(breweryIndex, BreweryManager.getBrewery(breweryIndex));
+            setBrewery(breweryIndex, BreweryManager.getBrewery(breweryIndex));
         }
         // Randomly reorder the tour
         Collections.shuffle(tour);
     }
-    
+
     public void addBrewery(Brewery brewery) {
-    	if (brewery == null) return;
+        if (brewery == null) return;
         tour.add(brewery);
         fitness = 0;
         distance = 0;
         beerCount = 0;
     }
-    
+
     public void removeBrewery(Brewery brewery) {
-    	if (brewery == null) return;
+        if (brewery == null) return;
         tour.remove(brewery);
         fitness = 0;
         distance = 0;
@@ -63,7 +66,7 @@ public class Tour{
     }
 
     public Brewery getBrewery(int tourPosition) {
-    	if (tourPosition < 0 || tourPosition >= tourSize()) return null;
+        if (tourPosition < 0 || tourPosition >= tourSize()) return null;
         return tour.get(tourPosition);
     }
 
@@ -75,82 +78,66 @@ public class Tour{
         distance = 0;
         beerCount = 0;
     }
-    
+
     // Gets the tours fitness
     public double getFitness() {
         if (fitness == 0) {
-            fitness = 1/(double)getDistance();
+            fitness = 1 / (double) getDistance();
         }
         return fitness;
     }
-    
+
     // Get the total distance of the tour
-    public double getDistance(){
+    public double getDistance() {
         if (distance == 0) {
-            double tourDistance = 0;
-            for (int breweryIndex=0; breweryIndex < tourSize(); breweryIndex++) {
-                // Get brewery we're travelling from
-                Brewery fromBrewery = getBrewery(breweryIndex);
-                // Brewery we're travelling to
-                Brewery destinationBrewery;
-                // Check we're not on our tour's last brewery, if we are set our
-                // tour's final destination brewery to our starting brewery
-                if(breweryIndex+1 < tourSize()){
-                    destinationBrewery = getBrewery(breweryIndex+1);
-                }
-                else{
-                    destinationBrewery = getBrewery(0);
-                }
-                // Get the distance between the two cities
-                tourDistance += BreweryManager.getDistanceBetween(fromBrewery, destinationBrewery);
-            }
-            distance = tourDistance;
+            distance = IntStream.range(1, tour.size())
+                    .mapToDouble(i -> BreweryManager.getDistanceBetween(tour.get(i - 1), tour.get(i))).sum();
         }
         return distance;
     }
-    
+
     //Get total number of beer collected
     public int getBeerCount() {
-    	if (beerCount == 0) {
-    		beerCount = tour.stream().mapToInt(Brewery::getBeerCount).sum();
-    	}
-    	return beerCount;
+        if (beerCount == 0) {
+            beerCount = tour.stream().mapToInt(Brewery::getBeerCount).sum();
+        }
+        return beerCount;
     }
 
     public int tourSize() {
         return tour.size();
     }
-    
+
     // Check if the tour contains a brewery
-    public boolean containsBrewery(Brewery brewery){
+    public boolean containsBrewery(Brewery brewery) {
         return tour.contains(brewery);
     }
-    
+
     @Override
     public String toString() {
-    	if (tour == null) return "";
-		StringBuilder sb = new StringBuilder();
-		sb.append(String.format("Found %s beer factories", tour.size()-2));
-		for (int i = 0; i < tour.size(); i++) {
-			double distBetween = 0;
-			if (i != 0) distBetween = BreweryManager.getDistanceBetween(getBrewery(i-1), getBrewery(i));
-			sb.append(System.lineSeparator());
-			sb.append(String.format("\t%s distance %.1fkm", tour.get(i), distBetween));
-		}
-		sb.append(System.lineSeparator());
-		sb.append(String.format("Total distance travelled: %.1fkm", getDistance()));
-		sb.append(System.lineSeparator());
-		sb.append(System.lineSeparator());
-		sb.append(String.format("Collected %s beer types:", tour.stream().mapToInt(Brewery::getBeerCount).sum()));
-		for (Brewery brew : tour) {
-			if (brew == null || brew.getBeerList() == null) continue;
-			for (Beer beer : brew.getBeerList()) {
-				sb.append(System.lineSeparator());
-				sb.append("\t"+beer.toString());
-			}
-		}
-		sb.append(System.lineSeparator());
-		
-		return sb.toString();
+        if (tour == null) return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Found %s beer factories", tour.size() - 2));
+        for (int i = 0; i < tour.size(); i++) {
+            double distBetween = 0;
+            if (i != 0) distBetween = BreweryManager.getDistanceBetween(getBrewery(i - 1), getBrewery(i));
+            sb.append(System.lineSeparator());
+            sb.append(String.format("\t%s distance %.1fkm", tour.get(i), distBetween));
+        }
+        sb.append(System.lineSeparator());
+        sb.append(String.format("Total distance travelled: %.1fkm", getDistance()));
+        sb.append(System.lineSeparator());
+        sb.append(System.lineSeparator());
+        sb.append(String.format("Collected %s beer types:", tour.stream().mapToInt(Brewery::getBeerCount).sum()));
+        for (Brewery brew : tour) {
+            if (brew == null || brew.getBeerList() == null) continue;
+            for (Beer beer : brew.getBeerList()) {
+                sb.append(System.lineSeparator());
+                sb.append("\t" + beer.toString());
+            }
+        }
+        sb.append(System.lineSeparator());
+
+        return sb.toString();
     }
 }
